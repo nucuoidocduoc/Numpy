53d7921ca4d0e41140109f0c5f6e0a13cde39ece
phuong_nt <nguyenphuongbka@gmail.com>
2019-12-04 21:59:21 +0700

    ok

---
+++ Source_Exercise/anExample.py
@@ -0,0 +1,18 @@
+# import numpy as np
+
+# a=np.arange(15).reshape(3,5)
+
+# a.shape
+
+# a.ndim
+
+# a.size
+
+# a.itemsize
+
+# a.dtype
+
+
+import numpy as np
+c = np.arange(144).reshape(3, 4, 3, 4)
+print(c)
